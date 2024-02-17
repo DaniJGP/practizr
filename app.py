1d32fcf24133700c59dadc55551d8f736b8b4153
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2024-02-17 19:07:21 -0300

    Basic html layout and flask route functionality

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template
 
 app = Flask(__name__)
+app.secret_key = b'I.zCPA"m,8TAkoy(}Cfc'
+
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+
+@app.route("/addrow", methods=["POST"])
+def addrow():
+    flash("You added the row")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
