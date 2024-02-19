d004dd07691c17fc045dbd53db5c269086e2cd1f
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2024-02-19 12:11:31 -0300

    Implemented simple add row functionality using request.form data

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask, flash, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+# TODO Read more about this secret key stuff
 app.secret_key = b'I.zCPA"m,8TAkoy(}Cfc'
 
+area_list = ["Technique", "Music Theory", "Repertoire", "Transcription", "Ear Training", "Improvisation"]
+practice_table = []
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", area_list=area_list)
 
 
-@app.route("/addrow", methods=["POST"])
+@app.route("/addrow", methods=['GET','POST'])
 def addrow():
-    flash("You added the row")
-    return render_template("index.html")
+    if request.method == 'POST':
+        practice_table.append(request.form)
+        return render_template("index.html", area_list=area_list, practice_table=practice_table)
+    else:
+        return redirect("/")
 
 
 if __name__ == '__main__':
---
+++ static/core.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let area_select = document.querySelector('#area')
+    area_select.addEventListener('change', function(event) {
+        console.log("area value changed");
+        /*TODO function to */
+    })
+})
